38e7d017719208933bdea57c1d20628a46ad742f
Alex <alex.testa@studio.unibo.it>
2021-04-26 22:33:46 +0200

    Add PlayerBuilder and Leaderboard Test

---
+++ Testa/TestaTests/test/LeaderboardImplTestsLeaderboard.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Testa.Tests
+{
+    [TestClass()]
+    public class LeaderboardImplTestsLeaderboard
+    {
+        private LeaderboardImpl leaderboard;
+        private Dictionary<string, int> testLeaderboard;
+
+        [TestInitialize]
+        public void TestInitLeaderboard()
+        {
+            this.leaderboard = new LeaderboardImpl();
+            this.testLeaderboard = new Dictionary<string, int>();
+
+            this.leaderboard.AddPlayer("Alex", 1200);
+            this.leaderboard.AddPlayer("Marco", 1400);
+            this.leaderboard.AddPlayer("Massimo", 1800);
+
+            this.testLeaderboard.Add("Alex", 1200);
+            this.testLeaderboard.Add("Marco", 1400);
+            this.testLeaderboard.Add("Massimo", 1800);
+        }
+
+        [TestMethod()]
+        public void AddPlayerTestLeaderboard() {
+            foreach (KeyValuePair<string, int> element in this.leaderboard.ranking) 
+            {
+                Console.WriteLine("Key = {0}, Value = {1}", element.Key, element.Value);
+                Assert.IsTrue(this.testLeaderboard.ContainsKey(element.Key));
+                Assert.IsTrue(this.testLeaderboard.ContainsValue(element.Value));
+            }
+        }
+
+        [TestMethod()]
+        public void TestOverWritePlayer() => Assert.ThrowsException<ArgumentException>(() => this.leaderboard.AddPlayer("Alex", 2000));
+
+    }
+}
---
+++ Testa/TestaTests/test/PlayerBuilderImplTestsPlayerBuilder.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Testa;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Testa.Tests
+{
+    [TestClass()]
+    public class PlayerBuilderImplTestsPlayerBuilder
+    {
+        private PlayerBuilderImpl builder;
+
+        [TestInitialize]
+        public void InitBuilder()
+        {
+            this.builder = new PlayerBuilderImpl();
+        }
+
+        [TestMethod()]
+        public void TestCorrectBuild()
+        {
+            try
+            {
+                this.builder.Alias("Alex");
+                this.builder.Score(2000);
+                this.builder.Life(3);
+                this.builder.MaxLife(3);
+                this.builder.Build();
+            }
+            catch (Exception ex) 
+            {
+                Assert.Fail("Failed Build : " + ex.Message);
+            }
+        }
+
+        [TestMethod()]
+        public void TestUncorrectBuildOverflowLife()
+        {
+            Assert.ThrowsException<ArgumentException>(() => 
+            {
+                this.builder.Score(2000);
+                this.builder.Life(50);
+                this.builder.Build();
+            });
+        }
+
+        [TestMethod()]
+        public void TestUncorrectBuildUnderflowLife()
+        {
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                this.builder.Score(2000);
+                this.builder.Life(-50);
+                this.builder.Build();
+            });
+        }
+
+        [TestMethod()]
+        public void TestUncorrectUnderflowScore()
+        {
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                this.builder.Score(-2000);
+                this.builder.Life(3);
+                this.builder.Build();
+            });
+        }
+
+        [TestMethod()]
+        public void TestNullAlias()
+        {
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                this.builder.Alias(null);
+                this.builder.Score(2000);
+                this.builder.Life(3);
+                this.builder.Build();
+            });
+        }
+    }
+}
